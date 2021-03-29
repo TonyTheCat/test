9f8cea6ddd64782c22cec08f53fcfc23e1afd6c0
Anton Shishenya <anton.shishenya@gmail.com>
2021-03-29 14:35:29 +0300

    Add pagination

---
+++ helpers/paginator.js
@@ -0,0 +1,17 @@
+function paginator(items, page_params) {
+    const page = page_params.page || 1;
+    const per_page = page_params.per_page || 5;
+    const offset = (page - 1) * per_page;
+
+    const paginatedItems = items.slice(offset).slice(0, per_page);
+    const total_pages = Math.ceil(items.length / per_page);
+    
+    return {
+        page,
+        total_pages: total_pages,
+        total_items: items.length,
+        data: paginatedItems
+    };
+}
+
+module.exports = paginator;
--- routes/videodata.all.get.js
+++ routes/videodata.all.get.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const errors = require('../helpers/errors');
+const paginator = require('../helpers/paginator');
 const axios = require('axios');
 
-router.get('/videodata',
+router.post('/videodata',
     errors.wrap(async (req, res, next) => {
+        const page_params  = req.body.pagination;
         const videoData = await axios.get(`https://${process.env.DATASOURCE_URL}`);
         if (!videoData) throw new errors.NotFound('Requested URL not found');
-        res.send(200);
+        res.send(paginator(videoData.data, page_params));
     }));
 
 module.exports = router;
