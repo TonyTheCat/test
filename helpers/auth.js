a56ae3b4b2a7d524e0da3ef56ce074722276a167
Anton Shishenya <anton.shishenya@gmail.com>
2021-03-29 18:14:22 +0300

    Fix auth

--- helpers/auth.js
+++ helpers/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const token = jwt.sign({ data: 'test' }, process.env.AUTH_KEY, { expiresIn: '1h' });
 
 module.exports = (req, res, next) => {
-    const userToken = req.header('authorization').split(" ")[1];
+    const userToken = req.header('authorization') && req.header('authorization').split(" ")[1];
     try {
         jwt.verify(userToken, process.env.AUTH_KEY);
         next();
